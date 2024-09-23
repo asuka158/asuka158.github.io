adb71fe3ba1736aed1b87624365baf76defaaac3
Asuka-46 <djiangkun@163.com>
2024-09-23 16:46:25 +0800

    Site updated: 2024-09-23 16:46:23

--- js/clearCache.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    for (var key in localStorage) {
-        if (key.startsWith("hexo-blog-encrypt#")) {
-            localStorage.removeItem(key);
-        }
-    }
-});
